5af4897e388be9a1a6972aa2b3fcf00389fdbb7e
alexandervincent1 <alexander.vincent@elev.ga.ntig.se>
2025-12-17 11:24:28 +0100

    helt färdigt

--- backend/obj/Debug/net9.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net9.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0868e2c901913cd33c78171df45d4a4dcd399b9e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2bf5929a68529918417892480b98412b6f3baf4")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
